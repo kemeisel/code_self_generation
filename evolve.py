2ea6abe90e4353bf36db57c964ff3f15e0149f90
Keith Meisel <Keith.Meisel@learninga-z.com>
2017-06-02 16:46:58 -0400

    Code cleanup; Terminate generation on mutant success; Reworked progenitor printout

--- evolve.py
+++ evolve.py
@@ -10,61 +10,58 @@ bestResult = 0;
 def addToBuildingBlocks(newValue):
     global buildingBlocks
     buildingBlocks.append(newValue)
-    #print(buildingBlocks)
+    print(buildingBlocks)
 
 def createNewLine():
-	line = ""
-	addLine = True
-	while addLine:
-		line += choice(buildingBlocks) + " "
-		addLine = randint(1, 4) >= 2 
-	return line
-
+    line = ""
+    addLine = True
+    while addLine:
+        line += choice(buildingBlocks) + " "
+        addLine = randint(1, 4) >= 2 
+    return line
 
 def mutate():
-	mutant = []
-	for line in progenitor:
-		mutation = choice(mutations)
-		if (mutation == "retain"):
-			mutant.append(line)
-		if (mutation == "duplicate"):
-			mutant.append(line)
-			mutant.append(line)
-		if (mutation == "addAbove"):
-			mutant.append(createNewLine())
-			mutant.append(line)
-		if (mutation == "addBelow"):
-			mutant.append(line)
-			mutant.append(createNewLine())
-	return mutant
-	
+    mutant = []
+    for line in progenitor:
+        mutation = choice(mutations)
+        if (mutation == "retain"):
+            mutant.append(line)
+        if (mutation == "duplicate"):
+            mutant.append(line)
+            mutant.append(line)
+        if (mutation == "addAbove"):
+            mutant.append(createNewLine())
+            mutant.append(line)
+        if (mutation == "addBelow"):
+            mutant.append(line)
+            mutant.append(createNewLine())
+    return mutant
 
-def printNewProgenitor(x, printLines = True):
-	print("Best Result: " + str(x))
-	print("Generation: " + str(generationNumber))
-	if printLines == True:
-	    for line in progenitor:
-		    print(line)
-	    #print()
+def printNewProgenitor(best, iteration, generation, printLines = True):
+    print('Best result: {}, Iteration: {}, Generation: {}'.format(str(best), str(iteration), str(generation)))
+    if printLines == True:
+        for line in progenitor:
+            print(line)
 
-def main(ancestorResult = 0):
-	global generationNumber
-	global progenitor
-	for i in range (0, 10000):
-		mutant = mutate()
-		x = 0
-		executable = "\n".join(mutant)
-		try:
-			exec(executable)
-		except:
-			continue
+def main(ancestorResult = 0, generation = 1):
+    global progenitor
+    for iterator in range (0, 10000):
+        mutant = mutate()
+        x = 0
+        executable = "\n".join(mutant)
+        try:
+            exec(executable)
+        except:
+            continue
+            
+        thisResult = x
 
-		if x > ancestorResult:
-			ancestorResult = x
-			generationNumber += 1
-			progenitor = mutant
-			printNewProgenitor(x)
-			addToBuildingBlocks(str(ancestorResult))
-			main(ancestorResult)
+        # 'executable' acts on thisResult
+        if thisResult > ancestorResult:
+            progenitor = mutant
+            printNewProgenitor(thisResult, iterator, generation)
+            addToBuildingBlocks(str(thisResult))
+            main(thisResult, generation+1)
+            break
 
 main()
