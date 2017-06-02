91896650649b9b460ab48aab73ba1530fec6a446
Keith Meisel <Keith.Meisel@learninga-z.com>
2017-06-02 13:28:21 -0400

    Wholesale replacement of original ev.py

---
+++ evolve.py
@@ -0,0 +1,59 @@
+from random import randint
+from random import choice
+
+buildingBlocks = ["x", "1", "+", "="]
+mutations = ["retain", "retain", "retain", "retain", "retain", "retain", "duplicate", "remove", "remove", "addAbove", "addBelow"]
+progenitor = [""]
+generationNumber = 0
+bestResult = 0;
+
+
+def createNewLine():
+	line = ""
+	addLine = True
+	while addLine:
+		line += choice(buildingBlocks) + " "
+		addLine = randint(1, 4) >= 2 
+	return line
+
+
+def mutate():
+	mutant = []
+	for line in progenitor:
+		mutation = choice(mutations)
+		if (mutation == "retain"):
+			mutant.append(line)
+		if (mutation == "duplicate"):
+			mutant.append(line)
+			mutant.append(line)
+		if (mutation == "addAbove"):
+			mutant.append(createNewLine())
+			mutant.append(line)
+		if (mutation == "addBelow"):
+			mutant.append(line)
+			mutant.append(createNewLine())
+	return mutant
+	
+
+def printNewProgenitor(x):
+	print("Best Result: " + str(x))
+	print("Generation: " + str(generationNumber))
+	for line in progenitor:
+		print(line)
+	print()
+
+
+for i in range (0, 10000):
+	mutant = mutate()
+	x = 0
+	executable = "\n".join(mutant)
+	try:
+		exec(executable)
+	except:
+		continue
+
+	if x > bestResult:
+		bestResult = x
+		generationNumber += 1
+		progenitor = mutant
+		printNewProgenitor(x)
